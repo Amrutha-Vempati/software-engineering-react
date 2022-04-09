a66a18678a914d874985a92e978e1b43db57c6b4
Amrutha Vempati <amrutha.vempati@gmail.com>
2022-04-09 19:19:54 -0400

    Modified profile component files to accomodate dislike feature

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,8 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "../../services/dislikes-service";
+import {useEffect, useState} from "react";
+
 const MyDislikes = () => {
+    const [dislikedTuits, setDislikedTutis] = useState([]);
+    const findTuitsIDislike = () =>
+        service.findAllTuitsDislikedByUser("me")
+            .then((tuits) => setDislikedTutis(tuits));
+    useEffect(findTuitsIDislike, []);
+
     return(
         <div>
-            <h1>My Dislikes</h1>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDislike}/>
         </div>
-    )
-}
+    );
+};
 export default MyDislikes;
